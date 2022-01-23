38c245805304a89ca625258dc80fbc2041619cac
MWANGIKENNEDY <kenmwan75@gmail.com>
2022-01-23 03:50:24 +0300

    Added sticky navigation bar

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
 const menuButton=document.querySelector('.fa.fa-align-justify');
 const menuLinks=document.querySelector('.links');
 
+
+
+
 menuButton.addEventListener('click',function(){
     menuLinks.classList.toggle('show');
 })
+
+window.addEventListener("scroll",function(){
+    var navbar=document.querySelector('nav');
+
+    navbar.classList.toggle("sticky",window.scrollY > 0);
+});
